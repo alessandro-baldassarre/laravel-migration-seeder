f21c5e69dc40c240855df385ad09de3e2616072f
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-05-13 17:53:37 +0200

    fill table trains with function in controller

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -4,7 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Train;
 class TrainController extends Controller
 {
-    //
+
+    public function index(){
+
+        //$this->fillTrainsTable('italo','bergamo','mestre','2022-05-13 10:00:00','2022-05-13 12:00:00', 376953, 20, 1, 0);
+
+        return view('trains.index');
+
+    }
+
+    public function fillTrainsTable($train_company,$departure_station,$arrival_station,$departure_time,$arrival_time, $train_code, $number_of_carriages, $in_time, $deleted){
+
+        $train = new Train();
+
+        $train->fill([
+            'train_company'=> $train_company,
+            'departure_station'=> $departure_station,
+            'arrival_station'=> $arrival_station,
+            'departure_time'=> $departure_time,
+            'arrival_time'=> $arrival_time,
+            'train_code'=> $train_code,
+            'number_of_carriages'=> $number_of_carriages,
+            'in_time'=> $in_time,
+            'deleted'=> $deleted
+        ]);
+
+        $train->save();
+    }
 }
--- app/Train.php
+++ app/Train.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Train extends Model
 {
-    //
+    protected $fillable = [
+        'train_company','departure_station','arrival_station','departure_time',
+        'arrival_time','train_code','number_of_carriages','in_time','deleted'
+    ];
 }
